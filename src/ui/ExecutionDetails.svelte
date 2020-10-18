53a42d2c663b2cd6eca1894d7da3ee0a046a8520
Johannes Zillmann <jz@datameer.com>
2020-10-18 20:18:22 +0200

    Toggle all/min metrics

--- src/ui/ExecutionDetails.svelte
+++ src/ui/ExecutionDetails.svelte
@@ -7,7 +7,13 @@
     let showAllMetrics = false;
 </script>
 
-<div class="text-lg font-bold">Plan</div>
+<div class="flex items-baseline">
+    <div class="text-lg font-bold">Plan</div>
+    <div class="ml-1">(</div>
+    <!-- svelte-ignore a11y-invalid-attribute -->
+    <a href="" on:click|preventDefault={() => (showAllMetrics = !showAllMetrics)}>{showAllMetrics ? 'all metrics' : 'min metrics'}</a>
+    <div>)</div>
+</div>
 <div class="flex flex-wrap">
     {#each sqlExecution.planNodes.filter((node) => showAllMetrics || node.metricsWithValues().length > 0) as planNode}
         <div class="ml-2">
